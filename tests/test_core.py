3104e0a009af1852c24720eb75cc1236166b1709
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-08-26 22:47:54 -0400

    added black

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,14 +1,18 @@
 import pytest
 from calculator.core import add, sub
 
+
 def test_add_simple():
     assert add(5, 3) == 8
+
 
 def test_add_negative():
     assert add(5, -2) == 3
 
+
 def test_sub_simple():
     assert sub(5, 3) == 2
+
 
 def test_sub_negative():
     assert sub(5, -2) == 7
